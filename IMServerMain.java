8601dd75deb8e2652294f9e764bead5b4e554d24
kzaiter <kzaiter@aol.com>
2015-07-15 14:54:40 -0400

    Create IMServerMain.java

---
+++ IMServerMain.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+
+public class IMServerMain {
+
+	public static void main(String[] args) {
+		IMServer twoThreeTwo = new IMServer();
+		twoThreeTwo.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		twoThreeTwo.startRunning();
+		
+	}
+}
